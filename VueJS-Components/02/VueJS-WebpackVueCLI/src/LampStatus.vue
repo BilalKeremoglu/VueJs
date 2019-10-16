ae723b1d07263334df2e79cdf0ec8bda118eff04
Bilal Keremoğlu <bilalkeremoglu@windowslive.com>
2019-10-16 09:59:15 +0300

    components

---
+++ VueJS-Components/02/VueJS-WebpackVueCLI/src/LampStatus.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Lamba Durumu : {{status}} !</h1>
+        <button @click="openLamp">Lambayı Aç</button>
+        <hr>
+    </div>
+</template>
+
+<script>
+export default {
+  data : function(){
+    return{
+      status : "Aktif"
+    }
+  },
+  methods :{
+        openLamp(){
+            this.status="açık.."
+        }
+  }
+}
+</script>
+
+<style>
+
+</style>
