8c54803fe9adb8afef3778d02d7f749151bb9ca1
Bilal Keremoğlu <bilalkeremoglu@windowslive.com>
2019-10-16 15:10:03 +0300

    component iletisimi

---
+++ VueJS-ComponentIletisim/componentler-arasi-iletisim-baslangic-kodu/src/components/User.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <h1>Parent Component (User)</h1>
+    <p>Burası parent component yani herşeyin import edildiği component :)</p>
+    <button @click="changeName">Adımı Değiştir</button>
+    <p>Child component üzerinden gelen veri : {{childData}}</p>
+    <hr>
+    <div class="row">
+      <app-user-detail :age="age" @data="childData= $event" :name="title"></app-user-detail>
+      <app-user-edit @ageWasEdited="age=$event" :age="age" :age2="age2"></app-user-edit>
+    </div>
+  </div>
+</template>
+<script>
+  import UserDetail from "./UserDetail";
+  import UserEdit from "./UserEdit";
+  export default {
+    components : {
+      appUserDetail : UserDetail,
+      appUserEdit : UserEdit,
+    },
+    data : function(){
+      return{
+        title:"Bilal",
+        childData:"",
+        age:25,
+        age2:35
+      }
+    },
+    methods:{
+      changeName : function(){
+        this.title="Bilal değil !"
+      }
+    }
+  }
+</script>
+
+<style>
+  div.container{
+    margin-top: 30px;
+    padding: 20px 40px;
+    background-color: #6a8d99;
+    border: 1px solid #666;
+  }
+  div.row {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+</style>
---
+++ VueJS-ComponentIletisim/componentler-arasi-iletisim-baslangic-kodu/src/components/UserEdit.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="col-md-s6">
+    <h3>Child Component 2</h3>
+    <p>Ben User.vue isimli Parent Component'in içerisindeki bir diğer Child componentim</p>
+    <p>Kullanıcı Yaşı :{{age2}}</p>
+    <p>{{age}}</p>
+    <button @click="changeAge">Yaş bilgisini değiştir.</button>
+  </div>
+</template>
+<script>
+export default{
+  props: {
+    age : {
+      type:String
+    },
+    age2 : {
+      type:String
+    }
+  },
+  methods: {
+    changeAge(){
+        this.$emit("ageWasEdited",30);
+      }
+  }
+}
+</script>
+
+<style scoped>
+  div {
+    background-color: lightgoldenrodyellow;
+    padding: 20px;
+    border: 1px solid #666;
+    display: inline-block;
+  }
+</style>
+
